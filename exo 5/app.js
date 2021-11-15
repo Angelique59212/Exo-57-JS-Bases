202bcf39da7c73447cd5f0da1f088d76936e41c5
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-11-15 14:48:27 +0100

    exo57 bases

---
+++ exo 5/app.js
@@ -0,0 +1,7 @@
+let number = prompt("donnez un nombre à virgule");
+let number2 = prompt("donnez un nouveau nombre à virgule");
+
+number = parseInt(number);
+
+let result = number * number2;
+    alert(result);
